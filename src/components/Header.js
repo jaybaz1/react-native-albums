0a58b29764024e7a8a6d03cc9100d0957767b732
Baz Jay <jaybaz1@gmail.com>
2018-03-14 21:57:19 +0000

    Added cardsection and renamed header file

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,8 +7,9 @@ const Header = (props) => {
 
     return (
         <View style={ viewStyle }>
-            <Text style={ textStyle }>{props.headerText}</Text>
+            <Text style={ textStyle }>{ props.headerText }</Text>
         </View>
+
     );
 };
 
@@ -21,7 +22,7 @@ const styles = {
         paddingTop: 15,
         shadowColor: '#000',
         shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.2,
+        shadowOpacity: 0.3,
         elevation: 5,
         position: 'relative'
     },
