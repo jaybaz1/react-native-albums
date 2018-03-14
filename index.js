bb6e98236c879d17f4dc07c23b4e1be008dd6120
Baz Jay <baz.jay@runpath.com>
2018-03-14 17:17:19 +0000

    Added album display components

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { View, AppRegistry } from 'react-native';
-import Header from './src/components/header';
+import Header from './src/components/Header';
 import AlbumList from './src/components/AlbumList';
 
 const App = () => (
